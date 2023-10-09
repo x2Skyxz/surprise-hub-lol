d1dbe20b82f20c298a20e2eea3de53af9a45e024
x2Skyxz_ <73633550+x2Skyxz@users.noreply.github.com>
2023-10-09 09:20:46 +0700

    Delete Maru Hub Premium BF.lua
    Private now lol

--- Maru Hub Premium BF.lua
+++
@@ -1,4 +0,0 @@
-getgenv().Key = "MARU-TGHMT-FZ42U-2201-G4IZ-M89M"
-getgenv().id = "634264626791448596"
-_G.Script_Language = "Thai"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
